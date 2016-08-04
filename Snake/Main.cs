c5c85ee799cbce69683514ed2d9feea4d2e3a0ce
Varun Parthasarathy <varun@parthasarathy.org>
2016-08-03 23:30:46 -0500

    initial commit

---
+++ Snake/Main.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Main : Form
+    {
+        private static readonly int WIDTH = 32, HEIGHT = 24, BOX_DIMEN = 25, BOX_MARGIN = 1;
+
+        private Panel panel;
+
+        private GameState gameState;
+        private bool paused;
+        private Snake game;
+        private Direction latestMove;
+        private int score;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //reset members
+            init();
+
+            //set window title
+            this.Text = "Snake";
+
+            //set the window inner dimensions and center it
+            this.ClientSize = new Size(800, 600);
+            this.CenterToScreen();
+
+            //Make the window non-resizeable
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //Set the keyboard input listener
+            this.KeyDown += Main_KeyDown;
+
+            //Setup panel for graphics
+            panel = new Panel();
+            panel.Name = "panel"; //specify control name, to access it in other parts of code
+            panel.Location = new Point(0, 0);
+            panel.Size = new Size(800, 600);
+            panel.BackColor = Color.White;
+            panel.Paint += Paint;
+            Controls.Add(panel);
+        }
+
+        private void init()
+        {
+            gameState = GameState.MENU;
+            latestMove = Direction.RIGHT;
+        }
+
+        //Draw to screen
+        private void Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == GameState.MENU)
+                DrawMenu(e);
+            else if (gameState == GameState.GAME)
+                DrawGame(e);
+            else if (gameState == GameState.END)
+                DrawGameOver(e); //TODO
+        }
+
+        private void DrawGameOver(PaintEventArgs paint)
+        {
+            Font myFont = new Font("Arial", 24);
+            paint.Graphics.DrawString("Game Over", myFont, Brushes.Blue, 50, 100);
+
+            myFont = new Font("Arial", 14);
+            paint.Graphics.DrawString("Your score: " + game.getLength(), myFont, Brushes.Green, 50, 150);
+
+            myFont = new Font("Arial", 14);
+            paint.Graphics.DrawString("Press any key to play again.", myFont, Brushes.Red, 50, 200);
+        }
+
+        //Draw the menu screen
+        private void DrawMenu(PaintEventArgs paint)
+        {
+            Font myFont = new Font("Arial", 24);
+            paint.Graphics.DrawString("Snake", myFont, Brushes.Blue, 50, 100);
+
+            myFont = new Font("Arial", 14);
+            paint.Graphics.DrawString("Press any key to play. (Arrows or WASD to move, Space or P to pause)", myFont, Brushes.Red, 50, 150);
+        }
+
+        //Draw the game screen
+        private void DrawGame(PaintEventArgs paint)
+        {
+            paint.Graphics.Clear(Color.White);
+
+            if(paused)
+            {
+                Font myFont = new Font("Arial", 24);
+                paint.Graphics.DrawString("Paused", myFont, Brushes.Blue, 50, 100);
+
+                myFont = new Font("Arial", 14);
+                paint.Graphics.DrawString("Press Space or P to resume.", myFont, Brushes.Red, 50, 150);
+
+                return;
+            }
+
+            drawBox(paint, game.getFood().X, game.getFood().Y, Brushes.Blue);
+
+            foreach (Point p in game.getBody())
+                drawBox(paint, p.X, p.Y, Brushes.Red);
+        }
+
+        //Draw a box space on the grid
+        private void drawBox(PaintEventArgs paint, int x, int y, Brush brush)
+        {
+            int px = (x * BOX_DIMEN) + BOX_MARGIN;
+            int py = (y * BOX_DIMEN) + BOX_MARGIN;
+            int dimen = BOX_DIMEN - (2 * BOX_MARGIN);
+            paint.Graphics.FillRectangle(brush, px, py, dimen, dimen);
+        }
+
+        //Handle keyboard input
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState == GameState.MENU || gameState == GameState.END)
+                startGame();
+
+            else if (gameState == GameState.GAME)
+                processGameInput(sender, e);
+        }
+
+        //process input events when in game
+        private void processGameInput(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Space || e.KeyData == Keys.P)
+                pauseControl();
+
+            else if (e.KeyData == Keys.Up || e.KeyData == Keys.W)
+                move(Direction.UP);
+            else if (e.KeyData == Keys.Down || e.KeyData == Keys.S)
+                move(Direction.DOWN);
+            else if (e.KeyData == Keys.Left || e.KeyData == Keys.A)
+                move(Direction.LEFT);
+            else if (e.KeyData == Keys.Right || e.KeyData == Keys.D)
+                move(Direction.RIGHT);
+        }
+
+        private void move(Direction direction)
+        {
+            latestMove = direction;
+        }
+
+        //start the game
+        private void startGame()
+        {
+            paused = false;
+            game = new Snake(WIDTH, HEIGHT);
+            gameState = GameState.GAME;
+
+            Thread gameThread = new Thread(this.gameLoop);
+            gameThread.Start();
+        }
+
+        private void gameLoop()
+        {
+            //Game loop
+            while (!game.isDead())
+            {
+                int sleepTime = getSleepTime(0);
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    if(!paused)
+                    {
+                        game.move(latestMove);
+                        updateScore(game.getLength());
+
+                        sleepTime = getSleepTime(game.getLength());
+                    }
+
+                    this.Refresh(); 
+                });
+
+                Thread.Sleep(sleepTime);
+            }
+            
+            this.Invoke((MethodInvoker)delegate
+            {
+                gameState = GameState.END;
+                this.Refresh();
+            });
+        }
+
+        private int getSleepTime(int length)
+        {
+            double f = 300.0 / (1 + (length / 10.0));
+            return (int)f;
+        }
+
+        private void updateScore(int score)
+        {
+            this.score = score;
+            this.Text = "Length: " + score;
+        }
+
+        //Handle puse controls
+        private void pauseControl()
+        {
+            paused = !paused;
+        }
+    }
+
+    public enum GameState
+    {
+        MENU,
+        GAME,
+        END
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Snake
+{
+    class Snake
+    {
+        private Queue<Point> body;
+        private Point food;
+        private int width, height;
+        private Random rand;
+        private bool duplicates;
+        private Direction prev;
+
+        public Snake(int width, int height)
+        {
+            rand = new Random();
+            body = new Queue<Point>();
+
+            this.width = width;
+            this.height = height;
+
+            duplicates = false;
+
+            randomize();
+            newFood();
+        }
+
+        private void randomize()
+        {
+            int x = rand.Next(4, width - 4);
+            int y = rand.Next(4, height - 4);
+            Point head = new Point(x, y);
+            body.Enqueue(head);
+        }
+
+        private void newFood()
+        {
+            int x = rand.Next(0, width);
+            int y = rand.Next(0, height);
+            food = new Point(x, y);
+
+            if (body.Contains(food))
+                newFood();
+        }
+
+        public Point getFood()
+        {
+            return food;
+        }
+
+        public Point[] getBody()
+        {
+            return body.ToArray();
+        }
+
+        public void move(Direction dir)
+        {
+            Point next = getMove(body.Last(), dir);
+            
+            if (body.Contains(next))
+                duplicates = true;
+
+            body.Enqueue(next);
+
+            if (isEating())
+                newFood();
+            else
+                body.Dequeue();
+
+            prev = dir;
+        }
+
+        private bool isEating()
+        {
+            return body.Last().Equals(food);
+        }
+
+        public bool isDead()
+        {
+            Point head = body.Last();
+
+            if (duplicates)
+                return true;
+
+            return head.X >= width || head.X < 0 || head.Y >= height || head.Y < 0;
+        }
+
+        public int getLength()
+        {
+            return body.Count;
+        }
+
+        private Point getMove(Point orig, Direction dir)
+        {
+            Point ret = new Point(orig.X, orig.Y);
+
+            if (dir == Direction.DOWN)
+                ret.Y += 1;
+            else if (dir == Direction.UP)
+                ret.Y -= 1;
+            else if (dir == Direction.LEFT)
+                ret.X -= 1;
+            else if (dir == Direction.RIGHT)
+                ret.X += 1;
+
+            return ret;
+        }
+    }
+    public enum Direction
+    {
+        UP = 0,
+        DOWN = 1,
+        LEFT = 2,
+        RIGHT = 3
+    }
+}
